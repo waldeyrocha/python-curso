11d1cd8cd815c9445754107dfd4e507399138fc1
Waldey <rochapereirawa@gmail.com>
2023-09-17 03:42:47 -0400

    Novo programa hello.py

--- hello.py
+++ hello.py
@@ -30,15 +30,17 @@ import os #Biblioteca externa
 
 current_language = os.getenv("LANG", "en_US")[:5]
 
-
 msg = "Hello, World!"
 
 if current_language == "pt_br":
-     
     msg = "Olá, Mundo!"
-
 elif current_language == "it_IT":
     msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+
+
+            
 
 
 
